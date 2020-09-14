d1d774b6b8bae02a636b05ba52c1fe59129d6382
Javier Llango <jallango1@espe.edu.ec>
2020-09-14 09:47:44 -0500

    modificaciones finales

--- src/main/java/com/system/green/house/security/LoginSuccessHandler.java
+++ src/main/java/com/system/green/house/security/LoginSuccessHandler.java
@@ -6,6 +6,7 @@ import java.util.Calendar;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.transaction.Transactional;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -15,7 +16,8 @@ import org.springframework.web.servlet.support.SessionFlashMapManager;
 
 @Component
 public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler  {
-
+	
+	@Transactional
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request,
 			HttpServletResponse response, Authentication authentication)
